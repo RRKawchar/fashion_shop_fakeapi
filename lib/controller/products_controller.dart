265c34f27f8588f68c98f8024d33707b5fbbfa66
rrk94 <riyzurrohmankawchar@gmail.com>
2023-05-20 21:51:02 +0600

    complete commit

--- lib/controller/products_controller.dart
+++ lib/controller/products_controller.dart
@@ -1,20 +1,28 @@
 import 'package:get/get.dart';
 import 'package:shop_app_getx/controller/product_repository.dart';
 
-class ProductsController extends GetxController{
-
-  ProductRepository productRepository=ProductRepository();
-  List products=[].obs;
-  productsController(){
+class ProductsController extends GetxController {
+  ProductRepository productRepository = ProductRepository();
+  List products = [].obs;
+  var isLoading = false.obs;
+  var isGridView=false.obs;
+  productsController() {
     productsFromApi();
   }
-  productsFromApi()async{
-    products=await productRepository.loadProductFromApi();
 
-
-    print(products);
+  productsFromApi() async {
+    isLoading(true);
+    products = await productRepository.loadProductFromApi();
+    isLoading(false);
   }
 
+  toggleGrid(){
+    isGridView(!isGridView.value);
+  }
 
-
+  @override
+  void onInit() {
+   productsFromApi();
+    super.onInit();
+  }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,11 +3,10 @@ import 'package:get/get.dart';
 import 'package:shop_app_getx/controller/products_controller.dart';
 
 class HomeScreen extends StatelessWidget {
-  final ProductsController productsController=Get.put(ProductsController());
+  final ProductsController productsController = Get.put(ProductsController());
   List categories = ["TShirt", "Pants", "Jeans", "Jackets", "Shirt"];
   @override
   Widget build(BuildContext context) {
-    productsController.productsController();
     return Scaffold(
       appBar: buildAppBar(),
       backgroundColor: Colors.grey[100],
@@ -18,37 +17,136 @@ class HomeScreen extends StatelessWidget {
             _buildTop(),
             _buildCategoriesRow(),
             Expanded(
-              child: ListView.builder(
-                padding: const EdgeInsets.only(top: 16),
-                itemCount: 20,
-                itemBuilder: (context,index){
-                  return Container(
-                    color: Colors.red,
-                    height: 150,
-                    margin: const EdgeInsets.only(bottom: 8),
-                    child: Row(
-                      children: [
-                        Container(
-                          width: 150,
-                          color: Colors.green,
-                        ),
-                        const SizedBox(width: 4,),
-
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
+              child: Obx(() {
+                if (productsController.isLoading.value) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (productsController.products.isEmpty) {
+                  return const Center(
+                    child: Text("No found Product"),
+                  );
+                }
+                if(productsController.isGridView.value){
+                  print("IsGridView ${productsController.isGridView.value}");
+                  return GridView.builder(
+                    padding: const EdgeInsets.only(top: 16),
+                    itemCount: productsController.products.length,
+                    itemBuilder: (context, index) {
+                      var product = productsController.products[index];
+                      return Card(
+                        elevation: 0,
+                        child: Container(
+                          height: 150,
+                          margin: const EdgeInsets.only(bottom: 8),
                           child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: const [
-                              Text("Title ",style: TextStyle(fontWeight: FontWeight.bold),),
-                              Text("Description "),
-                              Text("\$Price "),
+                            children: [
+                              Container(
+                                width: 140,
+                                height: 120,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: NetworkImage(
+                                          product['image'],
+                                        ),
+                                        fit: BoxFit.fill)),
+                              ),
+                              const SizedBox(
+                                width: 4,
+                              ),
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "${product['title']} ",
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      const SizedBox(
+                                        height: 10,
+                                      ),
+                                      Expanded(
+                                          child: Text(
+                                            "${product['description']}",
+                                            maxLines: 2,
+                                            overflow: TextOverflow.ellipsis,
+                                          )),
+                                      Text("\$${product['price']}"),
+                                    ],
+                                  ),
+                                ),
+                              )
                             ],
                           ),
-                        )
-                      ],
-                    ),
+                        ),
+                      );
+                    }, gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 0.7
+                  ),
                   );
-                },),
+                }
+                return ListView.builder(
+                  padding: const EdgeInsets.only(top: 16),
+                  itemCount: productsController.products.length,
+                  itemBuilder: (context, index) {
+                    var product = productsController.products[index];
+                    return Card(
+                      elevation: 0,
+                      child: Container(
+                        height: 150,
+                        margin: const EdgeInsets.only(bottom: 8),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 140,
+                              height: 120,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: NetworkImage(
+                                        product['image'],
+                                      ),
+                                      fit: BoxFit.fill)),
+                            ),
+                            const SizedBox(
+                              width: 4,
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "${product['title']} ",
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Expanded(
+                                        child: Text(
+                                      "${product['description']}",
+                                      maxLines: 3,
+                                      overflow: TextOverflow.ellipsis,
+                                    )),
+                                    Text("\$${product['price']}"),
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }),
             )
           ],
         ),
@@ -79,9 +177,11 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            productsController.toggleGrid();
+          },
           icon: const Icon(
-            Icons.filter_list_alt,
+            Icons.filter_list,
           ),
         )
       ],
